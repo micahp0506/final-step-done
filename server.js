51eb12ef28ab00788ac860524895694b33491cf6
cScholze <caleb.scholze@gmail.com>
2016-02-26 09:59:41 -0600

    setup app boilerplate

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// APP MIDDLEWARE
+app.set('view engine', 'jade');
+
+
+// ROUTE MIDDLEWARE
+app.get('/', (req,res) => {
+  res.render('index.jade');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening YO, on PORT ${PORT}`);
+});
