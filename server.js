b301a30ccf617e0dcd5ba46877f11ac6ec3119dd
Micah Wells <micahp0506@gmail.com>
2016-02-26 11:31:08 -0600

    Added register logic

--- server.js
+++ server.js
@@ -19,6 +19,37 @@ app.post('/login', (req, res) => {
   res.render('index.jade')
 });
 
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', (req, res) => {
+    if (req.body.password === req.body.verify) {
+        User.findOne({email: req.body.email}, (err, user) => {
+            if (err) throw (err);
+
+            if (user) {
+                res.redirect('/login');
+            } else {
+                User.create(req.body, (err) => {
+                    if (err) throw (err);
+
+                    res.redirect('/login');
+                });
+            }
+        });
+    }   else {
+            res.render('register', {
+                email: req.body.email,
+                message: 'Passwords do not match'
+            });
+        }
+});
+
 app.listen(PORT, () => {
   console.log(`Listening YO, on PORT ${PORT}`);
 });
